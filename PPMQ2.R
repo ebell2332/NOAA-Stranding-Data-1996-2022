74923a550390957bcabd78d32ec29a795f7fb035
ebell2332 <bellerinn2@gmail.com>
2026-01-30 12:20:09 -0500

    created new model that makes raster to map the intensities in the study area -filtered to specific years to map and maped with geom_tile -use patchwork() to combine plots into 1 figure

--- PPMQ2.R
+++ PPMQ2.R
@@ -1,3 +1,8 @@
+library(spatstat.geom)
+library(spatstat.explore)
+library(purrr)
+library(spatstat.model)
+
 q2 <- read_excel("/Users/ebell23/Downloads/strand_q2_allyrs.xlsx")
 
 
@@ -20,6 +25,7 @@ W_q2 <- as.owin(study_sf)
 q2_sf <- st_as_sf(q2, coords = c("lon", "lat"), crs = 4326, remove = FALSE)
 q2_sf <- st_transform(q2_sf, st_crs(study_sf))
 st_crs(q2_sf)
+
 
 #create points with overlap class (marks)
 q2_sf_filt <- q2_sf %>% mutate(overlap_class = case_when(
@@ -232,4 +238,168 @@ ggplot(cv_q2_tax_mn, aes(x = overlap_class, y = mean_cv, fill = overlap_class)) 
     axis.text = element_text(size = 10)      # Axis tick labels
   ) +
   theme(legend.position = "none")
+
+
+#2. Run Model with map output for CV pixels----
+cv2_q2 <- function(sf_data2, W, year_val, overlap_val, min_n = 50) {
+  
+  sub <- sf_data2 %>%
+    filter(
+      Year_num == year_val,
+      overlap_class == overlap_val
+    )
+  
+  n <- nrow(sub)
+  if (n < min_n) return(NULL)
+  
+  #fit ppm in same CRS as study area/W
+  xy <- st_coordinates(sub)
+  X  <- ppp(xy[,1], xy[,2], window = W)
+  
+  fit <- ppm(X ~ x + y)
+  
+  lam_im <- predict(fit, type = "trend") #predicted intensity surface
+  #Z <- as.numeric(predict(fit))
+  
+  
+  lam_vals <- as.vector(lam_im$v) #CV computed from predicted pixel values
+  lam_vals <- lam_vals[is.finite(lam_vals) & lam_vals > 0] #CV computed from predicted pixel values
+  cv_val <- sd(lam_vals) / mean(lam_vals)
+  
+  library(terra)
+  r_planar <- terra::rast(lam_im) #convert intensity surface to lat/long for consistent mapping
+  terra::crs(r_planar) <- sf::st_crs(study_sf)$wkt #assign CRS 
+  
+  r_ll <- project(r_planar, "EPSG:4326") #reproject to lat/long
+  
+  map_val <- as.data.frame(r_ll, xy = TRUE, na.rm = TRUE) #raster to df for ggplot
+  names(map_val) <- c("lon", "lat", "intensity")
+  
+
+  map_val <- map_val %>%
+    mutate(
+      Year_num = year_val,
+      overlap_class = overlap_val,
+      n = n,
+      cv_intensity = cv_val
+    )
+  
+  list(
+    summary = tibble(
+    Year_num = year_val,
+    overlap_class = overlap_val,
+    n = n,
+    cv_intensity = cv_val
+  ),
+  map = map_val
+  )
+}
+
+
+years_all <- sort(unique(q2_sf2$Year_num)) #sorting by years
+overlap <- sort(unique(q2_sf2$overlap_class)) #sorting "1" and "2plus"
+
+results2_q2 <- list()
+
+for (yr in years_all) {
+  for (ov in overlap) {
+    
+    res <- cv2_q2(
+      sf_data2 = q2_sf2,
+      W = W_q2,
+      year_val = yr,
+      overlap_val = ov,
+      min_n = 50 #minimum of 50 strandings in this year x overlap class
+    )
+    
+    if (!is.null(res)) {
+      results2_q2[[paste(yr, ov, sep = "_")]] <- res
+    }
+  }
+}
+
+cv2_df <- bind_rows(lapply(results2_q2, '[[', "summary"))
+map_val <- bind_rows(lapply(results2_q2, '[[', "map"))
+
+
+yr_spec <- 2017 #choose specific year to filter results to
+
+map_yr <- map_val %>%
+  filter(Year_num == yr_spec) #filter data set to one year
+
+
+ggplot(map_yr) + #plot the filtered year
+  geom_tile(aes(x = lon, y = lat, fill = intensity)) +
+  coord_fixed() +
+  scale_fill_viridis_c(name = "Intensity") +
+  facet_wrap(~ overlap_class, ncol = 1)+ #makes the overlap classes vertical
+  facet_grid(~overlap_class) + #makes overlap classes horizontal
+  theme_minimal()+
+  labs(fill = "Intensity", title = "Spatial stranding concentration in 2017 by overlap categories") +
+  theme_gray() #makes background grey
+
+#Multiple years (small maps, one per year)
+yrs_use <- 2015:2017
+
+map_multiyr <- map_val %>%
+  filter(Year_num == yrs_use)
+
+ggplot(map_multiyr) + #plot the filtered year
+  geom_tile(aes(x = lon, y = lat, fill = intensity)) +
+  coord_fixed() +
+  scale_fill_viridis_c(name = "Intensity") +
+  facet_grid(Year_num ~ overlap_class)+
+  theme_minimal()+
+  labs(fill = "Intensity", title = "Spatial stranding concentration in 2020 by overlap categories") +
+  theme_gray() #makes background grey
+
+#Multiple years, average mean per pixel across years
+  #average yearly ppm spatial prediction of each overlap class
+
+yrs_mult <- 2017:2022
+
+map_mean <- map_val %>%
+  filter(Year_num %in% yrs_mult) %>%
+  mutate(lon_r = round(lon, 3),
+         lat_r = round(lat, 3)) %>%
+  group_by(overlap_class, lon_r, lat_r) %>%
+  summarise(mean_intensity = mean(intensity, na.rm = TRUE),
+            n_years = n_distinct(Year_num),
+            .groups = "drop")
+
+map_mn <- ggplot(map_mean)+
+  geom_tile(aes(x = lon_r, y = lat_r, fill = mean_intensity)) +
+  facet_wrap(~overlap_class) +
+  coord_equal()+
+  scale_fill_viridis_c(name = "Average Intensity") +
+  theme_minimal()+
+  labs(title = "Average predicted spatial concentration from 2017-2022",
+       x = "Longitude", y = "Latitude") +
+  theme_gray()
+  
+yrs_mult1 <- 1996:2002
+
+map_mean1 <- map_val %>%
+  filter(Year_num %in% yrs_mult1) %>%
+  mutate(lon_r = round(lon, 3),
+         lat_r = round(lat, 3)) %>%
+  group_by(overlap_class, lon_r, lat_r) %>%
+  summarise(mean_intensity = mean(intensity, na.rm = TRUE),
+            n_years = n_distinct(Year_num),
+            .groups = "drop")
+
+map_mn1 <- ggplot(map_mean1)+
+  geom_tile(aes(x = lon_r, y = lat_r, fill = mean_intensity)) +
+  facet_wrap(~overlap_class) +
+  coord_equal()+
+  scale_fill_viridis_c(name = "Average Intensity") +
+  theme_minimal()+
+  labs(title = "Average predicted spatial concentration from 1996-2002",
+       x = "Longitude", y = "Latitude") +
+  theme_gray()
+
+
+library(patchwork)
+(map_mn1 + map_mn) + plot_layout(ncol =1)
+
 
